3388a8ef7930dbd48c75f4dc16c7bd34abaac808
rajabboy <coder@rajabboys-MacBook-Pro.local>
2024-03-30 17:51:58 +0500

    my new satise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/home/Home";
+import Navbar from "./components/layots/navbar/Navbar";
+import Footer from "./components/layots/footer/Footer";
+import NotPage from "./components/pages/notpage/NotPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/pages/notpage/NotPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./NotPage.module.css";
+function NotPage() {
+  return (
+    <div>
+      <h1>NotPage</h1>
+    </div>
+  );
+}
+
+export default NotPage;
